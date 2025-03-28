6d4513d02180d6e2248759d09816ebd0da620567
Gyulo94 <qkrcjfrb112@gmail.com>
2025-03-28 08:03:13 -0300

    style : layout 비율 수정

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -8,10 +8,10 @@ export default function RootLayout({
 }>) {
   return (
     <div className="flex w-full h-screen">
-      <div className="w-[20%] sticky top-0 left-0 h-screen">
+      <div className="w-[15%] sticky top-0 left-0 h-screen">
         <Sidebar />
       </div>
-      <div className="w-full flex flex-col px-5">
+      <div className="w-[85%] flex flex-col px-5">
         <Header />
         <main>{children}</main>
       </div>
