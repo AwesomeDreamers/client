048c382838c8401d2771b3a5b277154d74f3a7c1
Gyulo94 <qkrcjfrb112@gmail.com>
2025-03-29 07:55:35 -0300

    style : 사이드바가 화면을 따라오지 않는 부분 수정

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -7,8 +7,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <div className="flex w-full h-screen">
-      <div className="hidden xl:block xl:w-[15%] sticky top-0 left-0 h-screen">
+    <div className="flex w-full min-h-screen">
+      <div className="hidden xl:block xl:w-[15%] sticky top-0 h-screen">
         <Sidebar />
       </div>
       <div className="w-full xl:w-[85%] flex flex-col px-5">
